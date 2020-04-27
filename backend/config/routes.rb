533c668c6efffd350a2420dfbec564ff650a185d
Ryo Takazawa <ryotakazawaengineer@gmail.com>
2020-04-27 17:15:28 +0900

    Annotated routes info

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,74 @@
 # frozen_string_literal: true
 
+# == Route Map
+#
+#                                Prefix Verb       URI Pattern                                                                              Controller#Action
+#                      new_user_session GET        /api/users/sign_in(.:format)                                                             sessions#new {:format=>:json}
+#                          user_session POST       /api/users/sign_in(.:format)                                                             sessions#create {:format=>:json}
+#                  destroy_user_session DELETE     /api/users/sign_out(.:format)                                                            sessions#destroy {:format=>:json}
+#                     new_user_password GET        /api/users/password/new(.:format)                                                        devise/passwords#new {:format=>:json}
+#                    edit_user_password GET        /api/users/password/edit(.:format)                                                       devise/passwords#edit {:format=>:json}
+#                         user_password PATCH      /api/users/password(.:format)                                                            devise/passwords#update {:format=>:json}
+#                                       PUT        /api/users/password(.:format)                                                            devise/passwords#update {:format=>:json}
+#                                       POST       /api/users/password(.:format)                                                            devise/passwords#create {:format=>:json}
+#              cancel_user_registration GET        /api/users/cancel(.:format)                                                              devise/registrations#cancel {:format=>:json}
+#                 new_user_registration GET        /api/users/sign_up(.:format)                                                             devise/registrations#new {:format=>:json}
+#                edit_user_registration GET        /api/users/edit(.:format)                                                                devise/registrations#edit {:format=>:json}
+#                     user_registration PATCH      /api/users(.:format)                                                                     devise/registrations#update {:format=>:json}
+#                                       PUT        /api/users(.:format)                                                                     devise/registrations#update {:format=>:json}
+#                                       DELETE     /api/users(.:format)                                                                     devise/registrations#destroy {:format=>:json}
+#                                       POST       /api/users(.:format)                                                                     devise/registrations#create {:format=>:json}
+#                         users_current GET        /api/users/current(.:format)                                                             sessions#show {:format=>:json}
+#                new_admin_user_session GET        /admin/login(.:format)                                                                   active_admin/devise/sessions#new
+#                    admin_user_session POST       /admin/login(.:format)                                                                   active_admin/devise/sessions#create
+#            destroy_admin_user_session DELETE|GET /admin/logout(.:format)                                                                  active_admin/devise/sessions#destroy
+#               new_admin_user_password GET        /admin/password/new(.:format)                                                            active_admin/devise/passwords#new
+#              edit_admin_user_password GET        /admin/password/edit(.:format)                                                           active_admin/devise/passwords#edit
+#                   admin_user_password PATCH      /admin/password(.:format)                                                                active_admin/devise/passwords#update
+#                                       PUT        /admin/password(.:format)                                                                active_admin/devise/passwords#update
+#                                       POST       /admin/password(.:format)                                                                active_admin/devise/passwords#create
+#                            admin_root GET        /admin(.:format)                                                                         admin/dashboard#index
+#                       admin_dashboard GET        /admin/dashboard(.:format)                                                               admin/dashboard#index
+#        batch_action_admin_admin_users POST       /admin/admin_users/batch_action(.:format)                                                admin/admin_users#batch_action
+#                     admin_admin_users GET        /admin/admin_users(.:format)                                                             admin/admin_users#index
+#                                       POST       /admin/admin_users(.:format)                                                             admin/admin_users#create
+#                  new_admin_admin_user GET        /admin/admin_users/new(.:format)                                                         admin/admin_users#new
+#                 edit_admin_admin_user GET        /admin/admin_users/:id/edit(.:format)                                                    admin/admin_users#edit
+#                      admin_admin_user GET        /admin/admin_users/:id(.:format)                                                         admin/admin_users#show
+#                                       PATCH      /admin/admin_users/:id(.:format)                                                         admin/admin_users#update
+#                                       PUT        /admin/admin_users/:id(.:format)                                                         admin/admin_users#update
+#                                       DELETE     /admin/admin_users/:id(.:format)                                                         admin/admin_users#destroy
+#              batch_action_admin_users POST       /admin/users/batch_action(.:format)                                                      admin/users#batch_action
+#                           admin_users GET        /admin/users(.:format)                                                                   admin/users#index
+#                                       POST       /admin/users(.:format)                                                                   admin/users#create
+#                        new_admin_user GET        /admin/users/new(.:format)                                                               admin/users#new
+#                       edit_admin_user GET        /admin/users/:id/edit(.:format)                                                          admin/users#edit
+#                            admin_user GET        /admin/users/:id(.:format)                                                               admin/users#show
+#                                       PATCH      /admin/users/:id(.:format)                                                               admin/users#update
+#                                       PUT        /admin/users/:id(.:format)                                                               admin/users#update
+#                                       DELETE     /admin/users/:id(.:format)                                                               admin/users#destroy
+#                        admin_comments GET        /admin/comments(.:format)                                                                admin/comments#index
+#                                       POST       /admin/comments(.:format)                                                                admin/comments#create
+#                         admin_comment GET        /admin/comments/:id(.:format)                                                            admin/comments#show
+#                                       DELETE     /admin/comments/:id(.:format)                                                            admin/comments#destroy
+#         rails_mandrill_inbound_emails POST       /rails/action_mailbox/mandrill/inbound_emails(.:format)                                  action_mailbox/ingresses/mandrill/inbound_emails#create
+#         rails_postmark_inbound_emails POST       /rails/action_mailbox/postmark/inbound_emails(.:format)                                  action_mailbox/ingresses/postmark/inbound_emails#create
+#            rails_relay_inbound_emails POST       /rails/action_mailbox/relay/inbound_emails(.:format)                                     action_mailbox/ingresses/relay/inbound_emails#create
+#         rails_sendgrid_inbound_emails POST       /rails/action_mailbox/sendgrid/inbound_emails(.:format)                                  action_mailbox/ingresses/sendgrid/inbound_emails#create
+#          rails_mailgun_inbound_emails POST       /rails/action_mailbox/mailgun/inbound_emails/mime(.:format)                              action_mailbox/ingresses/mailgun/inbound_emails#create
+#        rails_conductor_inbound_emails GET        /rails/conductor/action_mailbox/inbound_emails(.:format)                                 rails/conductor/action_mailbox/inbound_emails#index
+#                                       POST       /rails/conductor/action_mailbox/inbound_emails(.:format)                                 rails/conductor/action_mailbox/inbound_emails#create
+#         rails_conductor_inbound_email GET        /rails/conductor/action_mailbox/inbound_emails/:id(.:format)                             rails/conductor/action_mailbox/inbound_emails#show
+#                                       PATCH      /rails/conductor/action_mailbox/inbound_emails/:id(.:format)                             rails/conductor/action_mailbox/inbound_emails#update
+#                                       PUT        /rails/conductor/action_mailbox/inbound_emails/:id(.:format)                             rails/conductor/action_mailbox/inbound_emails#update
+#                                       DELETE     /rails/conductor/action_mailbox/inbound_emails/:id(.:format)                             rails/conductor/action_mailbox/inbound_emails#destroy
+# rails_conductor_inbound_email_reroute POST       /rails/conductor/action_mailbox/:inbound_email_id/reroute(.:format)                      rails/conductor/action_mailbox/reroutes#create
+#                    rails_service_blob GET        /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
+#             rails_blob_representation GET        /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
+#                    rails_disk_service GET        /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
+#             update_rails_disk_service PUT        /rails/active_storage/disk/:encoded_token(.:format)                                      active_storage/disk#update
+#                  rails_direct_uploads POST       /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
+
 Rails.application.routes.draw do
   scope :api, defaults: { format: :json } do
     devise_for :users, controllers: { sessions: 'sessions' }
