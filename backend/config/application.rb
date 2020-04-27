e6378b213c543e835ad4b9724a266f9e75e45313
Ryo Takazawa <ryotakazawaengineer@gmail.com>
2020-04-27 17:26:37 +0900

    rubocopped

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -55,11 +55,11 @@ module Backend
 
     # For RSpec
     config.generators do |g|
-      g.test_framework :rspec, 
-            view_specs: false, 
-            helper_specs: false, 
-            controller_specs: false, 
-            routing_specs: false
+      g.test_framework :rspec,
+                       view_specs: false,
+                       helper_specs: false,
+                       controller_specs: false,
+                       routing_specs: false
     end
   end
 end
