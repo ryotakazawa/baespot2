078db953778aae4c05b17f052dfe3342c09c936f
Ryo Takazawa <ryotakazawaengineer@gmail.com>
2020-04-27 17:14:45 +0900

    Annotated schema info

--- backend/app/models/jwt_blacklist.rb
+++ backend/app/models/jwt_blacklist.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: jwt_blacklists
+#
+#  id  :bigint           not null, primary key
+#  exp :datetime         not null
+#  jti :string           not null
+#
+# Indexes
+#
+#  index_jwt_blacklists_on_jti  (jti)
+#
 class JwtBlacklist < ApplicationRecord
   include Devise::JWT::RevocationStrategies::Blacklist
   end
